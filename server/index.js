06da0b72d172c51df0632d7d9d65f79e69e2e361
ArielRenée <rios94.a@gmail.com>
2020-04-17 21:10:26 -0700

    CRA, added DB proxy to pjson, clean CRA

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const MoodifyDB = require('./src/moodify-db');
 
-// DEFAULT CONST
+/* DEFAULT CONST */
 const PORT = process.env.PORT || 3001;
 const DEFAULT_DB_NAME = 'moodify_db';
 const dbName = process.env.DB_NAME || DEFAULT_DB_NAME;
@@ -12,14 +12,14 @@ const db = new MoodifyDB(dbName);
 
 const app = express();
 
-// MIDDLEWARE
+/* MIDDLEWARE */
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 app.use(cors({ origin: true, credentials: true }));
 
-// ROUTES
+/* ROUTES */
 
 /* CHECK DATABASE CONNECTION */
 db.dbConnectionCheck().then(() => {
---
+++ server/moodify-db.js
@@ -0,0 +1,21 @@
+const pgp = require('pg-promise')();
+
+class MoodifyDatabase {
+  constructor(name) {
+    const connectionString = `postgres://localhost:5432/${name}`;
+    this.db = pgp(connectionString);
+  }
+
+  dbConnectionCheck() {
+    console.log('Testing database connection...');
+    return this.getMoodsCount().then((count) =>
+      console.log(`Found ${count} moods.`)
+    );
+  }
+
+  getMoodsCount() {
+    return this.db.one('SELECT count(*) FROM moods').then((m) => m.count);
+  }
+}
+
+module.exports = MoodifyDatabase;
