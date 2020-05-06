47dd646eba521523fe8c3bffa0fd5d3b2cfb51bc
ArielRenée <rios94.a@gmail.com>
2020-05-05 20:00:38 -0700

    ALAS git status | Cleaner UI | Affirmation button plays onclick

--- client/src/components/AffirmationsList.js
+++ client/src/components/AffirmationsList.js
@@ -2,24 +2,22 @@ import React from 'react';
 import { ListGroup, ListGroupItem } from 'reactstrap';
 
 const AffirmationsList = (props) => {
-  // const onClick = (affirmation) => {
-  //   var x = affirmation.id;
-  //   x.volume = 0.05;
-  //   x.play();
-  // };
+  const onClick = (i) => {
+    const audios = document.getElementsByClassName('audio');
+    audios.item(i).play();
+  };
 
   return (
     <div className='AffirmationsList'>
       <ListGroup>
         {props.affirmations.map((affirmation, i) => (
-          <ListGroupItem key={i} tag='button' action>
+          <ListGroupItem key={i} tag='button' action onClick={() => onClick(i)}>
             {affirmation.affirmation}
-            <audio controls>
-              <source
-                src={`/affirmations/${affirmation.id}.mp3`}
-                type='audio/mpeg'
-              />
-            </audio>
+            <audio
+              className={'audio'}
+              src={`/affirmations/${affirmation.id}.mp3`}
+              type='audio/mpeg'
+            />
           </ListGroupItem>
         ))}
       </ListGroup>
